e7f04002388fc63be85c6c175de6158fa8be3382
Marco 9726 <marcosarra9@gmail.com>
2023-01-09 19:00:55 +0100

    esercizio base svolto

--- js/script.js
+++ js/script.js
@@ -1,16 +1,23 @@
 const {
     createApp
-} = Vue;
+} = Vue
 
 createApp({
 
     data(){
         return {
-            number: null,
+            arrayMails: []
         }
     },
-    mounted(){
-        
-    }
+    methods: {
+        generateMails(){
+            for(i=0; i<10; i++){
+                axios.get('https://flynn.boolean.careers/exercises/api/random/mail').then((result) => {
+                    this.arrayMails.push(result.data.response)
+                })
+            }
+            console.log(this.arrayMails);
+        }
+    },
 
-}).mount('#app')
+}, ).mount('#app')
