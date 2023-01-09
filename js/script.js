5b580d3b6cc559741ed1c789d7dbdd321b273000
Marco 9726 <marcosarra9@gmail.com>
2023-01-09 19:20:01 +0100

    bonus svolto

--- js/script.js
+++ js/script.js
@@ -6,14 +6,21 @@ createApp({
 
     data(){
         return {
-            arrayMails: []
+            // array delle mail inizialmente vuoto
+            arrayMails: [],
         }
     },
     methods: {
+        //creo il metodo per generare le 10 mail e inserirle nell'array
         generateMails(){
+            // ciclo for con 10 iterazioni 
             for(i=0; i<10; i++){
+                // utilizzo l'API per generare una mail casuale 
                 axios.get('https://flynn.boolean.careers/exercises/api/random/mail').then((result) => {
-                    this.arrayMails.push(result.data.response)
+                    // definisco la mail in una costante 
+                    const mail = result.data.response;
+                    // pusho la mail generata nell'array 
+                    this.arrayMails.push(mail)
                 })
             }
             console.log(this.arrayMails);
